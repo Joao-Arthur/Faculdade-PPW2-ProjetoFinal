775d04803b0e2d7b63f5b464956d0fcfc937fc63
Joao-Arthur <joao.lothamer@gmail.com>
2021-06-30 21:48:59 -0300

    band resource validation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import healthcheck from './resources/healthcheck';
 import avaliableRoutes from './resources/avaliableRoutes';
 import album from './resources/album';
@@ -13,6 +14,9 @@ setupConnection();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.use('/healthcheck', healthcheck);
 app.use('/album', album);
 app.use('/band', band);
@@ -22,12 +26,36 @@ app.use('/', avaliableRoutes);
 app.listen(PORT, () => {
     console.log(
         `Server started on port: ${PORT}
-    Avaliable routes
-    /healthcheck
-    /album
-    /band
-    /user
-    /user/login
-    /`
+
+Avaliable routes
+
+/healthcheck
+/album
+/band
+/user
+/user/login
+/`
     );
 });
+
+/*
+fetch('/band', {
+    method: 'POST',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+            name: 'The Beatles',
+            members: [
+                'John Lennon',
+                'Paul McCartney',
+                'George Harrison',
+                'Ringo Starr'
+            ],
+            foundation: 1960,
+            dissolution: 1970
+        })
+});
+*/
--- src/resources/band/band.controller.ts
+++ src/resources/band/band.controller.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { Request } from 'express';
 const router = express.Router();
+
+type bandType = {
+    name: String;
+    members: String[];
+    foundation: Number;
+    dissolution: Number;
+    albums: String[];
+};
 
 router.get('/', (req, res) => {
     const name = req.query.name?.toString();
@@ -12,7 +20,30 @@ router.get('/', (req, res) => {
     res.send([1, 2, 3]);
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req: Request<{}, {}, bandType>, res) => {
+    const band = req.body;
+    try {
+        if (!band.name) throw new Error();
+        if (!band.members) throw new Error();
+        if (!band.foundation) throw new Error();
+        if (!band.dissolution) throw new Error();
+        if (!band.albums) throw new Error();
+
+        if (typeof band.name !== 'string') throw new Error();
+        if (!Array.isArray(band.members)) throw new Error();
+        if (typeof band.foundation !== 'number') throw new Error();
+        if (typeof band.dissolution !== 'number') throw new Error();
+        if (!Array.isArray(band.albums)) throw new Error();
+
+        if (!band.name.length) throw new Error();
+        if (!band.members.length) throw new Error();
+        if (!(band.foundation > 0)) throw new Error();
+        if (!(band.dissolution > 0)) throw new Error();
+        if (!band.albums.length) throw new Error();
+    } catch {
+        return res.sendStatus(422);
+    }
+
     res.send([1, 2, 3]);
 });
 
